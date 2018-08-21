0c9258e3eb19940dfe50bf84a29cbf9bf4eab6f8
Unknown <s215169611@nmmu.ac.za>
2018-08-21 15:11:46 +0200

    created project

---
+++ ProgressTracker/ProgressTracker/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ProgressTracker.MvcApplication" Language="C#" %>
---
+++ ProgressTracker/ProgressTracker/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ProgressTracker.Startup))]
+namespace ProgressTracker
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
